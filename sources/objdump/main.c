189f41a3b3a38c57f1005e4355bf696ebb48449c
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-02-12 09:17:55 +0100

    add project structure to repo

---
+++ sources/objdump/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_2018_nmobjdump
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("OBJDUMP\n");
+	return 0;
+}
